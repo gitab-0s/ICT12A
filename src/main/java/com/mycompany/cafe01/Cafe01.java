ef3f8ac7689696fc5c3ab65007432049c416f6a1
KiraSilent <jahnsen.blain.sangil@gmail.com>
2025-04-22 09:12:52 +0800

    try commit

---
+++ src/main/java/com/mycompany/cafe01/Cafe01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cafe01;
+
+/**
+ *
+ * @author CL3-PC11
+ */
+public class Cafe01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
