daf799cd97c337a4d78976f245bbe2e1b23f7318
KiraSilent <jahnsen.blain.sangil@gmail.com>
2025-04-22 11:52:51 +0800

    ACS

--- src/main/java/com/mycompany/cafe01/Login.java
+++ src/main/java/com/mycompany/cafe01/Login.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.cafe01;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author CL3-PC11
@@ -27,22 +29,95 @@ public class Login extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        p1 = new javax.swing.JPasswordField();
+        j1 = new javax.swing.JTextField();
+        text = new javax.swing.JLabel();
+        text1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel2.setFont(new java.awt.Font("Segoe UI Black", 3, 48)); // NOI18N
-        jLabel2.setText("LE BAGSAK");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 0, -1, 70));
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("KAPE BARAKUU");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, 70));
+        getContentPane().add(p1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 250, 210, 30));
+        getContentPane().add(j1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, 210, 30));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 3, 48)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("LE BAGSAK");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 0, 330, 80));
+        text.setFont(new java.awt.Font("Serif", 1, 24)); // NOI18N
+        text.setForeground(new java.awt.Color(0, 0, 0));
+        text.setText("PASSWORD");
+        getContentPane().add(text, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, -1, -1));
+
+        text1.setFont(new java.awt.Font("Serif", 1, 24)); // NOI18N
+        text1.setForeground(new java.awt.Color(0, 0, 0));
+        text1.setText("I.D");
+        getContentPane().add(text1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, -1, -1));
+
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
+        jButton1.setText("LOGIN");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 310, -1, -1));
+
+        jButton2.setForeground(new java.awt.Color(0, 0, 0));
+        jButton2.setText("CANCEL");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 310, -1, -1));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\CL3-PC11\\Downloads\\360_F_595090766_1wz5nknXYjZIiHHN2AkbNKCP5C0gzl7r.jpg")); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 60, 530, 300));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setEnabled(false);
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 770, 420));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        
+       
+       int a = Integer.parseInt(j1.getText());
+       int b = Integer.parseInt(p1.getText());
+       
+       
+       if (
+               a==1&&b==123)
+       {
+           JOptionPane.showMessageDialog(this, "LOGIN SUCCESS");
+           new Menu().setVisible(true);
+          
+       }
+       else
+               {
+                   this.dispose();
+               }
+       
+        
+
+
+
+
+
+   
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,7 +155,14 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField j1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField p1;
+    private javax.swing.JLabel text;
+    private javax.swing.JLabel text1;
     // End of variables declaration//GEN-END:variables
 }
